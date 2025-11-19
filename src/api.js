44580f7eea099b4c31e78e43b85ba9c549231e7d
Phichayapa Phemphoon <phichayapa.phemphoon@gmail.com>
2025-11-19 20:36:58 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ export const detectBanana = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
 
-  const res = await axios.post(`${BASE_URL}/predict`, formData, {
+  const res = await axios.post(`${BASE_URL}/detect`, formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
