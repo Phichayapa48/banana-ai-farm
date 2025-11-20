e7cfc1dec0e448816c0ca0d48721f98a1a78f656
Phichayapa Phemphoon <phichayapa.phemphoon@gmail.com>
2025-11-20 12:29:19 +0700

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,17 +1,13 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.VITE_API_BASE ||
-  "https://banana-ai-farm-production.up.railway.app";
+const BASE_URL = import.meta.env.VITE_API_BASE;
 
-export const detectBanana = async (file) => {
+export const detectBanana = async (file: Blob) => {
   const formData = new FormData();
   formData.append("file", file);
 
   const res = await axios.post(`${BASE_URL}/detect`, formData, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
+    headers: { "Content-Type": "multipart/form-data" },
   });
 
   return res.data;
