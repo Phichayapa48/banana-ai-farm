d5d2da6572a508d8926732c1df281685dd8d6559
Phichayapa Phemphoon <phichayapa.phemphoon@gmail.com>
2025-11-20 12:23:22 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // สำคัญมาก
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
@@ -12,7 +7,9 @@ export default defineConfig(({ mode }) => ({
     },
   },
   define: {
-    // ให้ Vite อ่าน env variable ของ Pages
-    "import.meta.env.VITE_API_BASE": JSON.stringify(process.env.VITE_API_BASE || "https://banana-ai-farm-production.up.railway.app")
+    "import.meta.env.VITE_API_BASE": JSON.stringify(
+      process.env.VITE_API_BASE ||
+        "https://banana-ai-farm-production.up.railway.app"
+    ),
   },
 }));
